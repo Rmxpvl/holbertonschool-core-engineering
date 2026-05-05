488f29dad4c4f1bf8357f7be0635d9a6239943d7
Rmxpvl <remy.pinville@gmail.com>
2026-05-05 19:27:35 +0200

    Refactor last_digit.py to improve readability of the conditional statements

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -6,4 +6,5 @@ if Last_digit > 5:
     print(f"Last digit of {number} is {Last_digit} and is greater than 5")
 elif Last_digit == 0:
     print(f"Last digit of {number} is {Last_digit} and is 0")
-else:    print(f"Last digit of {number} is {Last_digit} and is less than 6 and not 0")
+elif Last_digit < 6 and Last_digit != 0:
+    print(f"Last digit of {number} is {Last_digit} and is less than 6 and not 0")
