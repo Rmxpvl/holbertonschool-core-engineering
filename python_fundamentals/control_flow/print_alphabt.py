2156a3bbe8cd0c359c11541c8b8af69b9e0b17dd
Rmxpvl <remy.pinville@gmail.com>
2026-05-06 03:21:40 +0200

    Refactor print_alphabt.py to accumulate results in a string and print once

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+result = ""
 for i in range(97, 123):
     lettre = chr(i)
     if lettre == 'q' or lettre == 'e':
         continue
-    print(lettre, end="")
-    
+    result += lettre
+print(result)
