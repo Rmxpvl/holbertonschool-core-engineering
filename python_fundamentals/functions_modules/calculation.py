189c0535f4c38da927d657293c161f99aa98ec02
Rmxpvl <remy.pinville@gmail.com>
2026-05-08 22:12:42 +0200

    Add calculator functions for basic arithmetic operations

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import calculator_1
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print(calculator_1.add(a, b))
+    print(calculator_1.sub(a, b))
+    print(calculator_1.mul(a, b))
+    print(calculator_1.div(a, b))
