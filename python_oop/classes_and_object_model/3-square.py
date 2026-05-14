c6bde2f1694977104de579409138e6697f25e9a5
Rmxpvl <remy.pinville@gmail.com>
2026-05-14 16:05:41 +0200

    Refactor Square class to implement property for size attribute and enhance validation in setter

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -6,22 +6,15 @@ class Square:
     """Defines a square."""
 
     def __init__(self, size=0):
-        self.size = size
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = value
+        self.__size = size
 
     def area(self):
+        """Returns area of square"""
         return self.__size * self.__size
