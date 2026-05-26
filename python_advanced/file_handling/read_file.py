08717f58b35ebee698bf2f98f6f1115fffc7ab88
Rmxpvl <remy.pinville@gmail.com>
2026-05-26 10:12:27 +0200

    Update docstring for read_file function to clarify its purpose

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """Read a UTF-8 text file and print its content to stdout."""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
