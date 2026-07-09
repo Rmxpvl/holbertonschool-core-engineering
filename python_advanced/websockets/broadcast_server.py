c32a1b5235dd575612fd16928888dbb9f317b00a
Rmxpvl <remy.pinville@gmail.com>
2026-07-09 10:12:12 +0200

    Refactor connection_handler in broadcast_server.py to improve code readability

--- python_advanced/websockets/broadcast_server.py
+++ python_advanced/websockets/broadcast_server.py
@@ -6,15 +6,16 @@ import websockets
 clients = set()
 
 async def connection_handler(websocket):
+
+
     clients.add(websocket)
+
     try:
         async for message in websocket :
             for client in clients:
                 await client.send("B:" + message)
     finally:
         clients.remove(websocket)
-
-
 
 async def main():
     async with websockets.serve(connection_handler, "localhost", 8765):
