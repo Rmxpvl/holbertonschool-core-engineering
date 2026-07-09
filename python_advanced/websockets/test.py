96b8f80f5b2a9d3cb4809498a40ebb16e5057e05
Rmxpvl <remy.pinville@gmail.com>
2026-07-09 09:37:01 +0200

    Add unicast_server.py for WebSocket server implementation

---
+++ python_advanced/websockets/test.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+def convert_age(value):
---
+++ python_advanced/websockets/unicast_server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+clients = set()
+
+async def connection_handler(websocket):
+    clients.add(websocket)
+    try:
+        async for message in websocket :
+            for client in clients:
+                await client.send("U:" + message)
+    finally:
+        clients.remove(websocket)
+
+
+
+async def main():
+    async with websockets.serve(connection_handler, "localhost", 8765):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
