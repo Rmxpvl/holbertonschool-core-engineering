e52e30303b1ae4c508985b82e3ffef478a8eed68
Rmxpvl <remy.pinville@gmail.com>
2026-07-13 13:15:56 +0200

    Implement WebSocket server and client functionality with basic message handling

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
-from starlette.applications import Starlette
-from starlette.responses import HTMLResponse
-from starlette.routing import Route, WebSocketRoute
+import asyncio
+import os
+import sys
+import websockets
 
 
-async def homepage(request):
-    return HTMLResponse("<h1>WebSocket App</h1>")
+async def connect_and_send(uri, message):
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(message)
+
+        response = await websocket.recv()
+
+        return response
 
 
-async def websocket_endpoint(websocket):
-    await websocket.accept()
+if __name__ == "__main__":
+    uri = os.environ.get("WS_URI", "ws://localhost:8765")
 
-    while True:
-        message = await websocket.receive_text()
-        await websocket.send_text(message)
+    if len(sys.argv) > 1:
+        message = sys.argv[1]
+    else:
+        message = os.environ.get("WS_MESSAGE", "demo")
 
+    result = asyncio.run(connect_and_send(uri, message))
 
-app = Starlette(
-    routes=[
-        Route("/", homepage),
-        WebSocketRoute("/ws", websocket_endpoint),
-    ]
-)
+    sys.stdout.write(result)
