face343d8fb06404c541d121f652f382b2869d83
Rmxpvl <remy.pinville@gmail.com>
2026-07-13 13:13:36 +0200

    Implement WebSocket client functionality and add HTML template for user input

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 
-import asyncio
-import os
-import sys
-import websockets
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.routing import Route, WebSocketRoute
 
 
-async def connect_and_send(uri, message):
-    async with websockets.connect(uri) as websocket:
-        await websocket.send(message)
-
-        response = await websocket.recv()
-
-        return response
+async def homepage(request):
+    return HTMLResponse("<h1>WebSocket App</h1>")
 
 
-if __name__ == "__main__":
-    uri = os.environ.get("WS_URI", "ws://localhost:8765")
+async def websocket_endpoint(websocket):
+    await websocket.accept()
 
-    if len(sys.argv) > 1:
-        message = sys.argv[1]
-    else:
-        message = os.environ.get("WS_MESSAGE", "demo")
+    while True:
+        message = await websocket.receive_text()
+        await websocket.send_text(message)
 
-    result = asyncio.run(connect_and_send(uri, message))
 
-    sys.stdout.write(result)
+app = Starlette(
+    routes=[
+        Route("/", homepage),
+        WebSocketRoute("/ws", websocket_endpoint),
+    ]
+)
